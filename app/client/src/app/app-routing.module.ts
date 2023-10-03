daa63040bc1f55c3a61dbf024d78b73538a09027
Basim Mohammed <107759928+Basimohd@users.noreply.github.com>
2023-10-03 15:32:42 +0530

    Angular Initial Page Setup

--- app/client/src/app/app-routing.module.ts
+++ app/client/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:()=> import('../app/pages/landing/landing.module').then(
+      (m)=> m.LandingModule
+      )
+  },
+  {
+    path:'login',
+    loadChildren:()=> import('../app/pages/auth/auth.module').then(
+      (m)=> m.AuthModule
+      )
+  },
+  {
+    path:'app',
+    loadChildren:()=> import('../app/pages/dashboard/dashboard.module').then(
+      (m)=> m.DashboardModule
+      )
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
