c2835866ff37e41a3ca69d381748422a5339e81c
shiyas03 <shiyastofficial@gmail.com>
2023-10-12 10:48:33 +0530

    navbar created

--- app/client/src/app/pages/landing/landing.module.ts
+++ app/client/src/app/pages/landing/landing.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { NavbarComponent } from 'src/app/components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
