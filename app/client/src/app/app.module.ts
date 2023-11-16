50cadbb265e62bc1196e43414a824e536a855058
shiyas03 <shiyastofficial@gmail.com>
2023-11-16 11:04:03 +0530

    component conflict fixed in app module file

--- app/client/src/app/app.module.ts
+++ app/client/src/app/app.module.ts
@@ -4,22 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
-
 import { FooterComponent } from './components/footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    NavbarComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
