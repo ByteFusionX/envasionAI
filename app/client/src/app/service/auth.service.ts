de35785ace275567f1cb0823a1e7196715fdedee
LIYANMUBARAK <115862601+LIYANMUBARAK@users.noreply.github.com>
2023-11-17 10:57:06 +0530

    user login completed

--- app/client/src/app/service/auth.service.ts
+++ app/client/src/app/service/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { signUpForm } from '../shared/models/signUpForm.interface';
-
+import { loginForm } from '../shared/models/loginForm.interface';
+import { loginResponse } from '../shared/models/loginResponse.interface';
+import { Observable } from 'rxjs';
 
 
 
@@ -15,5 +17,9 @@ export class AuthService {
   signUp(details:signUpForm){
     console.log(details)
     return this.http.post('http://localhost:3000/signup',details)
+  }
+
+  login(details:loginForm){
+    return this.http.post('http://localhost:3000/login',details)
   }
 }
