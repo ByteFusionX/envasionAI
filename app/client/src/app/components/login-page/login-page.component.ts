c84b5387790130629cfff5319b99b2b4ca4f04b6
Diyouf <diyoufkv7@gmail.com>
2023-11-23 12:54:16 +0530

    Input field Updated

--- app/client/src/app/components/login-page/login-page.component.ts
+++ app/client/src/app/components/login-page/login-page.component.ts
@@ -16,39 +16,47 @@ export class LoginPageComponent implements OnInit {
   submit: boolean = false
   incorrectPasswordError: boolean = false
   userExistError: boolean = false
+  touchPass: boolean = true
 
   constructor(public fb: FormBuilder, public authService: AuthService, public router: Router) { }
-
-  ngOnInit(): void {
-
-  }
 
   loginForm = this.fb.group({
     email: patterns.email,
     password: patterns.password
   })
 
-  get form(){
+  ngOnInit(): void {
+  }
+
+
+
+  get form() {
     return this.loginForm.controls
   }
 
- 
+  onPasswordTouched() {
+    this.touchPass = false
+  }
+
+
 
   onSubmit() {
     this.submit = true
-    const details = this.loginForm.value as loginForm
-    this.authService.login(details).subscribe((res: loginResponse) => {
-      if (res.token) {
-        console.log(res)
-        localStorage.setItem('userId', res.id as string)
-        localStorage.setItem('userToken', res.token as string)
-        this.router.navigate(['/'])
-      } else if (res.incorrectPassword) {
-        this.incorrectPasswordError = true
-      } else if (res.userExistError) {
-        this.userExistError = true
-      }
+    if (this.loginForm.valid) {
+      const details = this.loginForm.value as loginForm
+      this.authService.login(details).subscribe((res: loginResponse) => {
+        if (res.token) {
+          console.log(res)
+          localStorage.setItem('userId', res.id as string)
+          localStorage.setItem('userToken', res.token as string)
+          this.router.navigate(['/'])
+        } else if (res.incorrectPassword) {
+          this.incorrectPasswordError = true
+        } else if (res.userExistError) {
+          this.userExistError = true
+        }
 
-    })
+      })
+    }
   }
 }
