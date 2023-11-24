6cd1224bd6ba8b3ae178c8f5b66d9735555bf619
shiyas03 <shiyastofficial@gmail.com>
2023-11-25 02:25:59 +0530

    heading animation added in home

--- app/client/src/app/components/home/home.component.ts
+++ app/client/src/app/components/home/home.component.ts
@@ -1,22 +1,42 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  windowHeight : number = 0
+  windowHeight: number = 0
+  textShow: boolean = true
+  timeInterval!: any
+  headings: string[] = ['Photo Generation.', 'Music Generation.', 'Video Generation.', 'Code Generation.','Chatbot.']
+  heading: String = this.headings[0]
 
   constructor(private _scroller: ViewportScroller) { }
 
+  ngOnInit(): void {
+    this.showHeadings(0)
+  }
+
+  showHeadings(index: number) {
+    if (this.textShow === true) {
+      if (index == this.headings.length) index = 0
+      this.heading = this.headings[index++]
+    }
+    setTimeout(() => {
+      this.textShow = !this.textShow
+      this.showHeadings(index)
+    }, 3000)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.timeInterval)
+  }
+
   onClickLink(value: string) {
     const tagValue = value
-    console.log(tagValue);
-    
     this._scroller.scrollToAnchor(tagValue)
   }
 }
