82bceeefa0a794743b555c69f38d4f9e4caa370a
Diyouf <diyoufkv7@gmail.com>
2023-11-25 14:11:18 +0530

    Interceptor Added in the Client Side

--- app/client/src/app/pages/auth/auth.module.ts
+++ app/client/src/app/pages/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { MatModule } from 'src/app/shared/modules/mat.module';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { UserHttpInterceptor } from 'src/app/interceptor/http.interceptor';
 
 
 @NgModule({
@@ -14,6 +16,9 @@ import { MatModule } from 'src/app/shared/modules/mat.module';
     CommonModule,
     AuthRoutingModule,
     MatModule
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: UserHttpInterceptor, multi: true }
   ]
 })
 export class AuthModule { }
