29a3dc95395d09078b50d7e5630ffe4368afaeec
shiyas03 <shiyastofficial@gmail.com>
2023-11-28 16:32:09 +0530

    drop down box content added

--- app/client/src/app/components/navbar/navbar-animation.ts
+++ app/client/src/app/components/navbar/navbar-animation.ts
@@ -1,7 +1,7 @@
 import { animate, state, style, transition, trigger } from "@angular/animations";
 
 export const navbarTrigger = trigger('dropDownAnimation', [
-    state('open', style({ height: '150px' })),
+    state('open', style({ height: '200px' })),
     state('close', style({ height: '0px', display: 'none' })),
     transition('close <=> open', animate('200ms linear'))
   ])
